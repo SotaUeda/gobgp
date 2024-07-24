0df3c2e5d30d91b3d46179d79ed9dec6a16be5c0
UedaSota <sota.ueda.003@gmail.com>
2024-07-24 23:32:32 +0900

    modi: test

--- peer/peer_test.go
+++ peer/peer_test.go
@@ -3,6 +3,7 @@ package peer
 import (
 	"context"
 	"testing"
+	"time"
 )
 
 func TestPeerCanTransitionToConnectState(t *testing.T) {
@@ -10,6 +11,14 @@ func TestPeerCanTransitionToConnectState(t *testing.T) {
 	peer := NewPeer(config)
 	peer.Start()
 	ctx := context.Background()
+	go func() {
+		remote_config, _ := ParseConfig("64512 127.0.0.2 65412 127.0.0.1 active")
+		remote_peer := NewPeer(remote_config)
+		remote_peer.Start()
+		remote_peer.Next(ctx)
+	}()
+	// remote_peer側の処理が進むことを保証するためのwait
+	time.Sleep(1 * time.Second)
 	peer.Next(ctx)
 	want := CONNECT
 	if want != peer.State {
