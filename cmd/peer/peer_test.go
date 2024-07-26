821ded579934ca5623d8e522116d3890829f4931
UedaSota <sota.ueda.003@gmail.com>
2024-07-26 18:16:36 +0900

    OpenStateまでのテストを実装

--- cmd/peer/peer_test.go
+++ cmd/peer/peer_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func TestPeerCanTransitionToConnectState(t *testing.T) {
-	config, _ := ParseConfig("64512 127.0.0.1 65413 127.0.0.2 active")
+	config, _ := ParseConfig("64512 127.0.0.1 64513 127.0.0.2 active")
 	peer := NewPeer(config)
 	peer.Start()
 	ctx := context.Background()
 	go func() {
-		remote_config, _ := ParseConfig("64512 127.0.0.2 65412 127.0.0.1 passive")
+		remote_config, _ := ParseConfig("64513 127.0.0.2 64512 127.0.0.1 passive")
 		remote_peer := NewPeer(remote_config)
 		remote_peer.Start()
 		remote_peer.Next(ctx)
@@ -21,6 +21,28 @@ func TestPeerCanTransitionToConnectState(t *testing.T) {
 	time.Sleep(1 * time.Second)
 	peer.Next(ctx)
 	want := CONNECT
+	if want != peer.State {
+		t.Errorf("Want: %d,  Peer State: %d", want, peer.State)
+	}
+	ctx.Done()
+}
+func TestPeerCanTransitionToOpenSentState(t *testing.T) {
+	config, _ := ParseConfig("64512 127.0.0.4 64513 127.0.0.5 active")
+	peer := NewPeer(config)
+	peer.Start()
+	ctx := context.Background()
+	go func() {
+		remote_config, _ := ParseConfig("64513 127.0.0.5 64512 127.0.0.4 passive")
+		remote_peer := NewPeer(remote_config)
+		remote_peer.Start()
+		remote_peer.Next(ctx)
+		remote_peer.Next(ctx) // イベントをenqueueできていない？
+	}()
+	// remote_peer側の処理が進むことを保証するためのwait
+	time.Sleep(1 * time.Second)
+	peer.Next(ctx)
+	peer.Next(ctx)
+	want := OPEN_SENT
 	if want != peer.State {
 		t.Errorf("Want: %d,  Peer State: %d", want, peer.State)
 	}
