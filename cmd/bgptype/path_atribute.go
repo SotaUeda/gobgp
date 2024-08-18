bbdb7c2e44c643c6f9b7e97bb80e3bd020c57489
UedaSota <sota.ueda.003@gmail.com>
2024-08-19 00:53:28 +0900

    NewUpdateMessageの実装

--- cmd/bgptype/path_atribute.go
+++ cmd/bgptype/path_atribute.go
@@ -1,17 +1,24 @@
 package bgptype
 
 import (
+	"fmt"
 	"net"
 )
 
-type PathAttribute struct {
-	Origin   Origin
-	AsPath   *AsPath
-	NextHop  net.IP
-	DontKnow []byte // 対応していないPathAtribute用
+// PathAttributeの種類
+// Origin
+// AsPathAttribute
+// NextHop
+// DontKnow	対応していないPathAtribute用
+type PathAttribute interface {
+	BytesLen() (uint16, error)
 }
 
 type Origin int
+
+func (o *Origin) BytesLen() (uint16, error) {
+	return 1, nil
+}
 
 const (
 	IGP Origin = iota
@@ -19,5 +26,64 @@ const (
 	INCOMPLETE
 )
 
-// TODO: BTreeの実装等は後回し
-type AsPath []AutonomousSystemNumber
+// AsPathはAsSequenceとAsSetの2つの型を持つ
+// AsSequenceはASを順番に並べたもの
+// AsSetは重複のないASの集合(集約用途)
+type AsPath interface {
+	BytesLen() uint16
+	Add() error
+	GetAs() ([]AutonomousSystemNumber, error)
+}
+
+type AsSequence []AutonomousSystemNumber
+
+func (seq *AsSequence) BytesLen() (uint16, error) {
+	asBytesLen := 2 * len(*seq)
+	// AsSetかAsSequenceかを表すoctet + ASの数を表すoctet + ASのbytesの値
+	return 1 + 1 + uint16(asBytesLen), nil
+}
+
+func (seq *AsSequence) Add(as AutonomousSystemNumber) error {
+	*seq = append(*seq, as)
+	return nil
+}
+
+func (seq *AsSequence) GetAs() []AutonomousSystemNumber {
+	return *seq
+}
+
+type AsSet map[AutonomousSystemNumber]struct{}
+
+func (set *AsSet) BytesLen() (uint16, error) {
+	asBytesLen := 2 * len(*set)
+	// AsSetかAsSequenceかを表すoctet + ASの数を表すoctet + ASのbytesの値
+	return 1 + 1 + uint16(asBytesLen), nil
+}
+
+func (set *AsSet) Add(as AutonomousSystemNumber) error {
+	if _, exists := (*set)[as]; exists {
+		return fmt.Errorf("AS %d already exists", as)
+	}
+	(*set)[as] = struct{}{}
+	return nil
+}
+
+func (set *AsSet) GetAs() []AutonomousSystemNumber {
+	keys := make([]AutonomousSystemNumber, 0, len(*set))
+	for key := range *set {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+type NextHop net.IP
+
+func (n *NextHop) BytesLen() (uint16, error) {
+	return 4, nil
+}
+
+type DontKnow []byte // 対応していないPathAtribute用
+
+func (d *DontKnow) BytesLen() (uint16, error) {
+	return uint16(len(*d)), nil
+}
--- cmd/packets/update.go
+++ cmd/packets/update.go
@@ -11,7 +11,7 @@ type UpdateMessage struct {
 	Header                              Header
 	WithdrawnRoutes                     []*net.IPNet
 	withdrawnRouteLen                   uint16 // ルート数ではなく、bytesにしたときのオクテット数
-	PathAttributes                      []*bgptype.PathAttribute
+	PathAttributes                      []bgptype.PathAttribute
 	pathAttributeLen                    uint16 // bytesにしたときのオクテット数
 	NetworkLayerReachabilityInformation []*net.IPNet
 	// NLRIのオクテット数はBGP UpdateMessageに含めず、
@@ -20,11 +20,48 @@ type UpdateMessage struct {
 }
 
 func NewUpdateMessage(
-	pa []*bgptype.PathAttribute,
+	pas []bgptype.PathAttribute,
 	nlri []*net.IPNet,
-	wr []*net.IPNet) *UpdateMessage {
-	//TODO
-	return &UpdateMessage{}
+	wr []*net.IPNet) (*UpdateMessage, error) {
+	paLen := uint16(0)
+	for _, pa := range pas {
+		l, err := pa.BytesLen()
+		if err != nil {
+			return nil, err
+		}
+		paLen += l
+	}
+	nlriLen := uint16(0)
+	for _, n := range nlri {
+		l, err := NetByteLen(n)
+		if err != nil {
+			return nil, err
+		}
+		nlriLen += l
+	}
+	wrLen := uint16(0)
+	for _, w := range wr {
+		l, err := NetByteLen(w)
+		if err != nil {
+			return nil, err
+		}
+		wrLen += l
+	}
+	hMinLen := uint16(19)
+	h := NewHeader(
+		// +4はpath_attribute_length(u16)と
+		// withdrawn_routes_length(u16)のbytes表現分
+		hMinLen+paLen+nlriLen+wrLen+4,
+		Update,
+	)
+	return &UpdateMessage{
+		Header:                              *h,
+		WithdrawnRoutes:                     wr,
+		withdrawnRouteLen:                   wrLen,
+		PathAttributes:                      pas,
+		pathAttributeLen:                    paLen,
+		NetworkLayerReachabilityInformation: nlri,
+	}, nil
 }
 
 func (u *UpdateMessage) Show() string {
@@ -47,4 +84,29 @@ func (u *UpdateMessage) ToBytes() ([]byte, error) {
 func (u *UpdateMessage) ToMessage(b []byte) error {
 	//TODO
 	return nil
+}
+
+// NetByteLenはプレフィックスからバイト長を返す
+// 0 => 1
+// 1~8 => 2
+// 9~16 => 3
+// 17~24 => 4
+// 25~32 => 5
+// TODO: routing.goと重複しているので、共通化する
+func NetByteLen(n *net.IPNet) (uint16, error) {
+	ones, _ := n.Mask.Size()
+	switch {
+	case ones == 0:
+		return 1, nil
+	case ones >= 1 && ones <= 8:
+		return 2, nil
+	case ones >= 9 && ones <= 16:
+		return 3, nil
+	case ones >= 17 && ones <= 24:
+		return 4, nil
+	case ones >= 25 && ones <= 32:
+		return 5, nil
+	default:
+		return 0, fmt.Errorf("invalid prefix length")
+	}
 }
